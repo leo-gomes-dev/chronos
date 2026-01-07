e5ab717465499746e52b2bdc6805f355fa312599
Leo Gomes <leogomesdeveloper@gmail.com>
2026-01-07 00:39:41 -0300

    footer-apoio

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,6 +4,10 @@ import { RouterLink } from '../RouterLink';
 export function Footer() {
   return (
     <footer className={styles.footer}>
+      <RouterLink href='https://mpago.la/1kz2Yay' target='_blank'>
+        <h3 className={styles.titleApoio}>Gostou do projeto?</h3>
+        <p>Se este trabalho te ajudou, considere me pagar um café!</p>
+      </RouterLink>
       <RouterLink href='/about-pomodoro/'>
         Entenda como funciona a técnica pomodoro
       </RouterLink>
